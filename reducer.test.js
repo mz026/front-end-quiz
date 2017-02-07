8a25467f78b8ed8130f7928ee49c97aa51ceca77
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2017-02-07 11:41:08 +0800

    init

---
+++ reducer.test.js
@@ -0,0 +1,8 @@
+import reducer from './reducer'
+import {expect} from 'chai'
+
+describe('Reducer', ()=> {
+  it('should be a function', ()=> {
+    expect(reducer).to.be.an.instanceof(Function)
+  })
+})
