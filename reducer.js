ef7baf0a779ce4aaf6e2b97d4822793e52b2d6d5
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2017-02-07 15:22:50 +0800

    add list case as sample

--- reducer.js
+++ reducer.js
@@ -9,7 +9,10 @@ export default function(state = defaultState, action) {
   switch(action.type) {
 
     case ActionType.GET_LIST_SUCCESS:
+      state.list = action.response
       return state
 
   }
 }
+
+export { defaultState }
--- reducer.test.js
+++ reducer.test.js
@@ -1,8 +1,22 @@
-import reducer from './reducer'
+import reducer, { defaultState } from './reducer'
+
+import * as actionCreators from './action'
+import * as ActionType from './action'
+import * as mockData from './mockData'
+
 import { expect } from 'chai'
 
 describe('Reducer', ()=> {
   it('should be a function', ()=> {
     expect(reducer).to.be.an.instanceof(Function)
+  })
+  describe('when GET_LIST_SUCCESS', ()=> {
+    it('merges data', ()=> {
+      let action = actionCreators.getList()
+
+      let newState = reducer(defaultState, action)
+
+      expect(newState.list).to.deep.equal(action.response)
+    })
   })
 })
